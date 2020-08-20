2f6c419acc7793c57d2ace6d79c8806050f24302
Daniel Alves <danielalves.ccomp@gmail.com>
2020-08-19 23:52:11 -0300

    changing the button text

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,38 +6,38 @@ import { addTodo, setTodoText, updateTodo } from '../actions';
 import Input from './Input';
 
 class TodoForm extends React.Component {
-  onChangeText(text) {
-    this.props.dispatchSetTodoText(text);
-  }
+	onChangeText(text) {
+		this.props.dispatchSetTodoText(text);
+	}
 
-  onPress() {
-    const { todo } = this.props;
-    if(todo.id) {
-      this.props.dispatchUpdateTodo(todo);
-    } else {
-      const { text } = todo;
-      this.props.dispatchAddTodo(text);
-    }
-  }
+	onPress() {
+		const { todo } = this.props;
+		if (todo.id)
+			return this.props.dispatchUpdateTodo(todo);
 
-  render () {
-    const { text } = this.props.todo; 
-    return (
-      <View style={styles.formContainer}>
-        <View style={styles.inputContainer}>
-          <Input 
-            onChangeText={text => this.onChangeText(text)}
-            value={text}
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button 
-            onPress={() => this.onPress()}
-            title="ADD" />
-        </View>
-      </View>
-    );
-  }
+		const { text } = todo;
+		this.props.dispatchAddTodo(text);
+	}
+
+	render() {
+		const { text, id } = this.props.todo;
+		return (
+			<View style={styles.formContainer}>
+				<View style={styles.inputContainer}>
+					<Input
+						onChangeText={text => this.onChangeText(text)}
+						value={text}
+					/>
+				</View>
+				<View style={styles.buttonContainer}>
+					<Button
+						onPress={() => this.onPress()}
+						title={id ? "SAVE" : "ADD"}
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
